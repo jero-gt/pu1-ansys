7ac21250598df7165523f15371cabbf1263f6795
Jeronimo Gomez Tantarelli <jeronimogomeztantarelli@yahoo.com.ar>
2023-05-24 03:24:51 +0000

    NO CAMBIE NADA A EFECTOS PRACTICOS. 1. Agregué el cerrar graficos (lo deje comentado, me parecio mejor hacerlo función porque es más sencillo de leer en el script) 2. Cambie algunas amplitudes y titulos de los graficos (agregué la variable lim_ejes_fase en plot complejo)
    COMENTARIO: Teniamos "mal graficada" la TFTD inicial. La habiamos graficado desde 0 hasta 110. Ahora se puede ver que toma valores negativos!! Se parece bastante a un sinc pero con una frecuencia muy muy rapida. 
    Para la fase de la TFTD de la señal de entrada elegi el intervalo [-0.5 0.5] para graficar en el "eje x" porque no se notaba bien la varación de la misma.
    Para los otros gráficos lo dejé de [-1 1]
    
    
    Descansa frittezzzz - spiiiceee

--- pu1.m
+++ pu1.m
@@ -6,15 +6,9 @@ clc, close all, clearvars;
 [n,x]=senial(736456);
 stemCompleto([n(1) n(end) -0.5 2.5],'n','Amplitud','Gráfico de la SVIC',25,'m*-',1.5,n,x);
 
-%Transformamos a la señal de entrada
-%ds = 0.001; s = [-2:ds:2]; X = zeros(size(s));
-%for i = 1:length(s)
-%X(i)=sum(x.*exp(-1i*2*pi*s(i)*n));
-%end
-
 [X, s] = TFTD(x, n);
-plotComplejo([s(1) s(end) 0 110], [s(1) s(end) 0 110], 'Frecuencia "s" ','Módulo de la TFTD','Gráfico de la TFTD',25,'m-',1.5,s,X);
-
+plotComplejo([-1 1 -80 110], [-1 1 0 110],[-0.5 0.5 -3 3], 'Frecuencia "s" ','Módulo de la TFTD','Gráfico de la TFTD',25,'m-',1.5,s,X);
+%cerrar_graficos;
 %% ECUACION EN DIFERENCIAS 1
 % y[n]= (1/2)*x[n] + (1/2)*x[n-1]
 n1=0:101; %Eje coordenado x para la ecuacion en diferencias 1
@@ -27,10 +21,12 @@ y1(length(x)+1)= (1/2)*x(length(x));
 %Respuesta impulsional al sistema
 h1=(1/2)*deltaK(n1) + (1/2)*deltaK(n1-1);
 H1 = 0.5*(1 + exp(-j*2*pi*s));
-stemCompleto([n(1) n(end) -0.5 2.5],'n','Amplitud','Gráfico de la SVIC ana',25,'m*-',1.5,n1,h1);
-plotComplejo([s(1) s(end) 0 110],[s(1) s(end) 0 2] ,'Frecuencia "s" ','Amplitud','H1 ana',25,'m-',1.5,s,H1);
+stemCompleto([n(1) n(end) 0 1],'n','Amplitud','h1[n] analitica',25,'m*-',1.5,n1,h1);
+plotComplejo([s(1) s(end) 0 1.5],[s(1) s(end) 0 1.5] ,[-1 1 -3 3],'Frecuencia "s" ','Amplitud','H1 analitica',25,'m-',1.5,s,H1);
 % Verificamos que coincida la rta imp. analatica y la de los sistemas.
 h1_v = s1(deltaK(n));
 [H1_v, s] = TFTD(h1_v,n1);
-stemCompleto([n(1) n(end) -0.5 2.5],'n','Amplitud','Gráfico de la SVIC',25,'m*-',1.5,n1,h1_v);
-plotComplejo([s(1) s(end) 0 110], [s(1) s(end) 0 2], 'Frecuencia "s" ','Módulo de la TFTD','Gráfico de la TFTD',25,'m-',1.5,s,H1_v);
+stemCompleto([n(1) n(end) 0 1],'n','Amplitud','h1[n] Matlab',25,'m*-',1.5,n1,h1_v);
+plotComplejo([s(1) s(end) 0 1.5], [s(1) s(end) 0 1.5], [-1 1 -3 3],'Frecuencia "s" ','Módulo de la TFTD','H1(f)=TFTD{h1[n]}',25,'m-',1.5,s,H1_v);
+
+%cerrar_graficos;
