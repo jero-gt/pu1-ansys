4b8cee5eb109ce245d2bef1fded97f5df336e957
Ignacio Brittez <ignacio_brittez@outlook.com>
2023-06-07 21:05:07 -0300

    Figuras del ej 2, funcion plotDoble y aplicarCambiosFigura

--- pu1_ej2.m
+++ pu1_ej2.m
@@ -4,23 +4,24 @@ clc; close all; clearvars;
 
 %%
 [n,h]= hcanald(726176);
-stemCompleto([n(1) n(end) -1.5 1.5],'n','Amplitud','Respuesta impulsional del sistema',25,'m*-',1.5,n,h);
+stemCompleto([n(1) n(end) -1.5 1.5],'n','Amplitud','Respuesta impulsional del sistema',25,'r-',1.5,n,h);
 h_v=sis_eco(deltaK(n));
-Le
+
 %% Sonido
 [x,fs]=audioread('audio.wav');
 t=0:length(x)-1;
-plotCompleto([0 60845 -1 1],'t','Amplitud','Sonido (entrada)',20,'r-',1.5,t, x);
 
 y=sis_eco(x);
-sound(y, fs);
-plotCompleto([0 60845 -1 1],'t','Amplitud','Sonido procesado (salida)',20,'r-',1.5,t, y);
+plotDoble([0 60845 -1 1], [0 60845 -1 1], 'n', 'Amplitud', "Audio original", "Audio procesado", 20, 'r-', 1.5, t, x, t, y)
+%%sound(y, fs);
 
 y2=sis_2ramas(y);
 plotCompleto([0 60845 -1 1],'t','Amplitud','2 ramas de retardo',20,'r-',1.5,t, y2);
+%%sound(y2, fs);
 
 y3=sis_3ramas(y);
 plotCompleto([0 60845 -1 1],'t','Amplitud','3 ramas de retardo',20,'r-',1.5,t, y3);
+%%sound(y3, fs);
 
 
 
